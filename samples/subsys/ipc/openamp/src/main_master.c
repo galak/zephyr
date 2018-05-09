5e82c55ea3b9b6f004b2d9c3ff761a41325ca8dd
Kumar Gala <kumar.gala@linaro.org>
2018-05-08 19:10:26 -0500

    Update for using new code on master
    use inbuilt lib
    
    start implement of slave side
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- samples/subsys/ipc/openamp/src/main_master.c
+++ samples/subsys/ipc/openamp/src/main_master.c
@@ -3,115 +3,239 @@
  * Copyright (c) 2018, Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
- *
- * This implements the master side of an OpenAMP system.
  */
 
 #include <zephyr.h>
+#include <ipm.h>
 #include <misc/printk.h>
 #include <device.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <init.h>
 
 #include <openamp/open_amp.h>
+#include <metal/device.h>
 
-#include "platform.h"
-#include "resource_table.h"
+#include "common.h"
 
-#define APP_TASK_STACK_SIZE (512)
+#define APP_TASK_STACK_SIZE (1024)
 K_THREAD_STACK_DEFINE(thread_stack, APP_TASK_STACK_SIZE);
 static struct k_thread thread_data;
 
-static struct rpmsg_channel *rp_channel;
-static struct rpmsg_endpoint *rp_endpoint;
+static struct device *ipm_handle = NULL;
 
-static K_SEM_DEFINE(channel_created, 0, 1);
+static metal_phys_addr_t shm_physmap[] = { SHM_START_ADDR };
+static struct metal_device shm_device = {
+	.name = SHM_DEVICE_NAME,
+	.bus = NULL,
+	.num_regions = 1,
+	{
+		{
+			.virt       = (void *) SHM_START_ADDR,
+			.physmap    = shm_physmap,
+			.size       = SHM_SIZE,
+			.page_shift = 0xffffffff,
+			.page_mask  = 0xffffffff,
+			.mem_flags  = 0,
+			.ops        = { NULL },
+		},
+	},
+	.node = { NULL },
+	.irq_num = 0,
+	.irq_info = NULL
+};
 
-static K_SEM_DEFINE(message_received, 0, 1);
 static volatile unsigned int received_data;
 
-static struct rsc_table_info rsc_info;
-static struct hil_proc *proc;
+static struct virtio_vring_info rvrings[2] = {
+	[0] = {
+		.info.align = VRING_ALIGNMENT,
+	},
+	[1] = {
+		.info.align = VRING_ALIGNMENT,
+	},
+};
+static struct virtio_device vdev;
+static struct rpmsg_virtio_device rvdev;
+static struct metal_io_region *io;
+static struct virtqueue * vq[2];
 
-static void rpmsg_recv_callback(struct rpmsg_channel *channel, void *data,
-				int data_length, void *private,
-				unsigned long src)
+static unsigned char virtio_get_status(struct virtio_device *vdev)
+{
+	return VIRTIO_CONFIG_STATUS_DRIVER_OK;
+}
+
+static void virtio_set_status(struct virtio_device *vdev, unsigned char status)
+{
+	unsigned char *status_byte = (void *)RSC_TABLE_ADDRESS;
+
+	*status_byte = status;
+
+	return ;
+}
+
+static uint32_t virtio_get_features(struct virtio_device *vdev)
+{
+	return 1 << VIRTIO_RPMSG_F_NS;
+}
+
+static void virtio_set_features(struct virtio_device *vdev,
+                                      uint32_t features)
+{
+	return ;
+}
+
+static void virtio_notify(struct virtqueue *vq)
+{
+	uint32_t dummy_data = 0x55005500; /* Some data must be provided */
+
+	ipm_send(ipm_handle, 0, 0, &dummy_data, sizeof(dummy_data));
+}
+
+struct virtio_dispatch dispatch = {
+	.get_status = virtio_get_status,
+	.set_status = virtio_set_status,
+	.get_features = virtio_get_features,
+	.set_features = virtio_set_features,
+	.notify = virtio_notify,
+};
+
+static K_SEM_DEFINE(data_sem, 0, 1);
+static K_SEM_DEFINE(data_rx_sem, 0, 1);
+
+static void platform_ipm_callback(void *context, u32_t id, volatile void *data)
+{
+	k_sem_give(&data_sem);
+}
+
+int endpoint_cb(struct rpmsg_endpoint *ept, void *data,
+                             size_t len, uint32_t src, void *priv)
 {
 	received_data = *((unsigned int *) data);
-	k_sem_give(&message_received);
+
+	k_sem_give(&data_rx_sem);
+
+	return RPMSG_SUCCESS;
 }
 
-static void rpmsg_channel_created(struct rpmsg_channel *channel)
+static K_SEM_DEFINE(ept_sem, 0, 1);
+
+struct rpmsg_endpoint my_ept;
+struct rpmsg_endpoint *ep = &my_ept;
+
+static void rpmsg_service_unbind(struct rpmsg_endpoint *ept)
 {
-	rp_channel = channel;
-	rp_endpoint = rpmsg_create_ept(rp_channel, rpmsg_recv_callback,
-				       RPMSG_NULL, RPMSG_ADDR_ANY);
-	k_sem_give(&channel_created);
+	(void)ept;
+	rpmsg_destroy_ept(ep);
 }
 
-static void rpmsg_channel_deleted(struct rpmsg_channel *channel)
+void ns_bind_cb(struct rpmsg_device *rdev, const char *name, uint32_t dest)
 {
-	rpmsg_destroy_ept(rp_endpoint);
+	(void)rpmsg_create_ept(ep, rdev, name,
+			RPMSG_ADDR_ANY, dest,
+			endpoint_cb,
+			rpmsg_service_unbind);
+
+	k_sem_give(&ept_sem);
 }
 
 static unsigned int receive_message(void)
 {
-	while (k_sem_take(&message_received, K_NO_WAIT) != 0)
-		hil_poll(proc, 0);
-	return received_data;
+        while (k_sem_take(&data_rx_sem, K_NO_WAIT) != 0) {
+                int status = k_sem_take(&data_sem, K_FOREVER);
+
+                if (status == 0) {
+			virtqueue_notification(vq[0]);
+                }
+        }
+        return received_data;
 }
 
 static int send_message(unsigned int message)
 {
-	return rpmsg_send(rp_channel, &message, sizeof(message));
+	return rpmsg_send(ep, &message, sizeof(message));
 }
+
+static struct rpmsg_virtio_shm_pool shpool;
 
 void app_task(void *arg1, void *arg2, void *arg3)
 {
 	ARG_UNUSED(arg1);
 	ARG_UNUSED(arg2);
 	ARG_UNUSED(arg3);
+	int status = 0;
+	struct metal_device *device;
 
 	printk("\r\nOpenAMP demo started\r\n");
 
 	struct metal_init_params metal_params = METAL_INIT_DEFAULTS;
-
 	metal_init(&metal_params);
 
-	proc = platform_init(RPMSG_MASTER);
-	if (proc == NULL) {
-		printk("platform_init() failed\n");
-		goto _cleanup;
+	status = metal_register_generic_device(&shm_device);
+	if (status != 0) {
+		printk("metal_register_generic_device(): could not register "
+		       "shared memory device: error code %d\n", status);
 	}
 
-	resource_table_init((void **) &rsc_info.rsc_tab, &rsc_info.size);
+        status = metal_device_open("generic", SHM_DEVICE_NAME, &device);
 
-	struct remote_proc *rproc_ptr = NULL;
-	int status = remoteproc_resource_init(&rsc_info, proc,
-					      rpmsg_channel_created,
-					      rpmsg_channel_deleted,
-					      rpmsg_recv_callback,
-					      &rproc_ptr, RPMSG_MASTER);
-	if (status != 0) {
-		printk("remoteproc_resource_init() failed with status %d\n",
+        if (status != 0) {
+                printk("metal_device_open failed %d\n", status);
+        }
+
+        io = metal_device_io_region(device, 0);
+
+	/* setup IPM */
+	ipm_handle = device_get_binding("MAILBOX_0");
+
+	ipm_register_callback(ipm_handle, platform_ipm_callback, NULL);
+
+	ipm_set_enabled(ipm_handle, 1);
+
+	/* setup vdev */
+	vq[0] = virtqueue_allocate(VRING_SIZE);
+	vq[1] = virtqueue_allocate(VRING_SIZE);
+
+	vdev.role = RPMSG_MASTER;
+	vdev.vrings_num = VRING_COUNT;
+	vdev.func = &dispatch;
+	rvrings[0].io = io;
+	rvrings[0].info.vaddr = (void *)VRING_TX_ADDRESS;
+	rvrings[0].info.num_descs = VRING_SIZE;
+	rvrings[0].info.align = VRING_ALIGNMENT;
+	rvrings[0].vq = vq[0];
+
+	rvrings[1].io = io;
+	rvrings[1].info.vaddr = (void *)VRING_RX_ADDRESS;
+	rvrings[1].info.num_descs = VRING_SIZE;
+	rvrings[1].info.align = VRING_ALIGNMENT;
+	rvrings[1].vq = vq[1];
+
+	vdev.vrings_info = &rvrings[0];
+
+	/* setup rvdev */
+	rpmsg_virtio_init_shm_pool(&shpool, (void *)SHM_START_ADDR, SHM_SIZE);
+	rpmsg_init_vdev(&rvdev, &vdev, ns_bind_cb, io, &shpool);
+
+	/* Since we are using name service, we need to wait for a response
+	 * from NS setup and than we need to process it
+	 */
+	k_sem_take(&data_sem, K_FOREVER);
+	virtqueue_notification(vq[0]);
+
+	/* Wait til nameservice ep is setup */
+	k_sem_take(&ept_sem, K_FOREVER);
+
+	unsigned int message = 0;
+	status = send_message(message);
+	if (status < 0) {
+		printk("send_message(%d) failed with status %d\n", message,
 		       status);
 		goto _cleanup;
 	}
 
-	while (k_sem_take(&channel_created, K_NO_WAIT) != 0)
-		hil_poll(proc, 0);
-
-	unsigned int message = 0;
-
-	status = send_message(message);
-	if (status < 0) {
-		printk("send_message(%d) failed with status %d\n",
-		       message, status);
-		goto _cleanup;
-	}
-
-	while (message <= 100) {
+	while (message < 100) {
 		message = receive_message();
 		printk("Primary core received a message: %d\n", message);
 
@@ -125,9 +249,7 @@ void app_task(void *arg1, void *arg2, void *arg3)
 	}
 
 _cleanup:
-	if (rproc_ptr) {
-		remoteproc_resource_deinit(rproc_ptr);
-	}
+	rpmsg_deinit_vdev(&rvdev);
 	metal_finish();
 
 	printk("OpenAMP demo ended.\n");
@@ -140,3 +262,15 @@ void main(void)
 			(k_thread_entry_t)app_task,
 			NULL, NULL, NULL, K_PRIO_COOP(7), 0, 0);
 }
+
+/* Make sure we clear out the status flag very early (before we bringup the
+ * secondary core) so the secondary core see's the proper status
+ */
+int init_status_flag(struct device *arg)
+{
+	virtio_set_status(NULL, 0);
+
+	return 0;
+}
+
+SYS_INIT(init_status_flag, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
