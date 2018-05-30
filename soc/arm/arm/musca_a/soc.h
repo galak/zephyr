450512693dfcdcdcc0bd145167209249097332fd
Karl Zhang <karl.zhang@linaro.org>
2018-05-30 17:52:32 +0800

    arm: v2m musca: Add support for v2m musca SoC and board
    Support musca board as a variant, it is a Cortex-M33 based SoC.
    
    Signed-off-by: Karl Zhang <karl.zhang@linaro.org>
    Signed-off-by: Minos Galanakis <minos.galanakis@arm.com>
    Signed-off-by: Andy Gross <andy.gross@linaro.org>
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

---
+++ soc/arm/arm/musca_a/soc.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC_H_
+#define _SOC_H_
+
+#ifndef _ASMLANGUAGE
+#include "system_cmsdk_musca.h"
+#endif
+
+#endif /* _SOC_H_ */
