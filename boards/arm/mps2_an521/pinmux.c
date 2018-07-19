1866093b178e831bb9c5441a582171f17bef1b96
Kumar Gala <kumar.gala@linaro.org>
2018-07-19 10:56:21 -0500

    add scc regs

--- boards/arm/mps2_an521/pinmux.c
+++ boards/arm/mps2_an521/pinmux.c
@@ -115,8 +115,77 @@
  * I2C_4_SCL : D41
  *
  */
+
+struct arm_scc_reg_map_t {
+    volatile uint32_t reset_ctrl;             /* 0x00 RW Reset Control Register */
+    volatile uint32_t clk_ctrl;               /* 0x04 RW Clock Control Register*/
+    volatile uint32_t pwr_ctrl;               /* 0x08 RW Power Control Register*/
+    volatile uint32_t pll_ctrl;               /* 0x0C RW Power Control Register */
+    volatile uint32_t dbg_ctrl;               /* 0x10 RW Debug Control Register */
+    volatile uint32_t sram_ctrl;              /* 0x14 RW SRAM Control Register */
+    volatile uint32_t intr_ctrl;              /* 0x18 RW Interupt Control Register */
+    volatile uint32_t reserved1;              /* 0x1C RW reserved */
+    volatile uint32_t cpu0_vtor_sram;         /* 0x20 RW Reset vector for CPU0 Secure Mode */
+    volatile uint32_t cpu0_vtor_flash;        /* 0x24 RW Reset vector for CPU0 Secure Mode */
+    volatile uint32_t cpu1_vtor_sram;         /* 0x28 RW Reset vector for CPU1 Secure Mode */
+    volatile uint32_t cpu1_vtor_flash;        /* 0x2C RW Reset vector for CPU0 Secure Mode */
+    volatile uint32_t iomux_main_insel;       /* 0x30 RW Main function in data select */
+    volatile uint32_t iomux_main_outse;       /* 0x34 RW Main function out data select */
+    volatile uint32_t iomux_main_oense;       /* 0x38 RW Main function out enable select */
+    volatile uint32_t iomux_main_default_in;  /* 0x3C RW Main function default in select */
+    volatile uint32_t iomux_altf1_insel;      /* 0x40 RW Alt function 1 in data select */
+    volatile uint32_t iomux_altf1_outse;      /* 0x44 RW Alt function 1 out data select */
+    volatile uint32_t iomux_altf1_oense;      /* 0x48 RW Alt function 1 out enable select */
+    volatile uint32_t iomux_altf1_default_in; /* 0x4C RW Alt function 1 default in select */
+    volatile uint32_t iomux_altf2_insel;      /* 0x50 RW Alt function 2 in data select */
+    volatile uint32_t iomux_altf2_outse;      /* 0x54 RW Alt function 2 out data select */
+    volatile uint32_t iomux_altf2_oense;      /* 0x58 RW Alt function 2 out enable select */
+    volatile uint32_t iomux_altf2_default_in; /* 0x5C RW Alt function 2 default in select */
+    volatile uint32_t pvt_ctrl;               /* 0x60 RW PVT control register */
+    volatile uint32_t spare0;                 /* 0x64 RW reserved */
+    volatile uint32_t iopad_ds0;              /* 0x68 RW Drive Select 0 */
+    volatile uint32_t iopad_ds1;              /* 0x6C RW Drive Select 1 */
+    volatile uint32_t iopad_pe;               /* 0x70 RW Pull Enable */
+    volatile uint32_t iopad_ps;               /* 0x74 RW Pull Select */
+    volatile uint32_t iopad_sr;               /* 0x78 RW Slew Select */
+    volatile uint32_t iopad_is;               /* 0x7C RW Input Select */
+    volatile uint32_t sram_rw_margine;        /* 0x80 RW reserved */
+    volatile uint32_t static_conf_sig0;       /* 0x84 RW Static configuration */
+    volatile uint32_t static_conf_sig1;       /* 0x88 RW Static configuration */
+    volatile uint32_t req_set;                /* 0x8C RW External Event Enable */
+    volatile uint32_t req_clear;              /* 0x90 RW External Event Clear */
+    volatile uint32_t iomux_altf3_insel;      /* 0x94 RW Alt function 3 in data select */
+    volatile uint32_t iomux_altf3_outse;      /* 0x98 RW Alt function 3 out data select */
+    volatile uint32_t iomux_altf3_oense;      /* 0x9C RW Alt function 3 out enable select */
+    volatile uint32_t iomux_altf3_default_in; /* 0xA0 RW Alt function 3 default in select */
+    volatile uint32_t pcsm_ctrl_override;     /* 0xA4 RW Q-Channels QACTIVE Override */
+    volatile uint32_t pd_cpu0_iso_override;   /* 0xA8 RW CPU0 Isolation Override */
+    volatile uint32_t pd_cpu1_iso_override;   /* 0xAC RW CPU1 Isolation Override */
+    volatile uint32_t sys_sram_rw_assist0;    /* 0xB0 RW CPU0 icache sram ldata */
+    volatile uint32_t sys_sram_rw_assist1;    /* 0xB4 RW CPU0 icache sram tag */
+    volatile uint32_t sys_sram_rw_assist2;    /* 0xB8 RW CPU1 icache sram ldata */
+    volatile uint32_t sys_sram_rw_assist3;    /* 0xBC RW CPU1 icache sram tag */
+    volatile uint32_t sys_sram_rw_assist4;    /* 0xC0 RW System sram */
+    volatile uint32_t sys_sram_rw_assist5;    /* 0xC4 RW System sram */
+    volatile uint32_t reserved2[3];           /* reserved */
+    volatile uint32_t crypto_sram_rw_assist0; /* 0xD4 RW Crypto ram */
+    volatile uint32_t crypto_sram_rw_assist1; /* 0xD8 RW Crypto sec sram */
+    volatile uint32_t crypto_sram_rw_assist2; /* 0xDC RW Reserved */
+    volatile uint32_t req_edge_sel;           /* 0xC0 RW Power clock request edge select */
+    volatile uint32_t req_enable;             /* 0xC4 RW Power clock request enable */
+    volatile uint32_t reserved3[28];          /* reserved */
+    volatile uint32_t chip_id;                /* 0x100 RO Chip ID 0x0797_0477 */
+    volatile uint32_t clock_status;           /* 0x104 RO Clock status */
+    volatile uint32_t io_in_status;           /* 0x108 RO I/O in status */
+};
+
+
+#define MUSCA_SCC_DEV \
+	((volatile struct arm_scc_reg_map_t *)0x5010C000)
+
 static void arm_mps2_pinmux_defaults(void)
 {
+	printk("in pinmux defaults\n");
 #if 0
 	u32_t gpio_0 = 0;
 	u32_t gpio_1 = 0;
