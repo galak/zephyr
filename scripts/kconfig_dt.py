a839bd8962baf9cf05c985bb3e2ab33170403b21
Kumar Gala <kumar.gala@linaro.org>
2018-11-15 12:30:34 -0600

    wip
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- scripts/kconfig_dt.py
+++ scripts/kconfig_dt.py
@@ -4,4 +4,27 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-print("yell")
+import sys
+import os
+import re
+
+# Types we support
+# 'string', 'int', 'hex', 'bool'
+gen_path="build/zephyr/include/generated/generated_dts_board.conf"
+
+regex = '^CONFIG_' + sys.argv[1] + '=*'
+
+print(os.getcwd())
+if os.path.isfile(gen_path):
+    while(1):
+        pass
+
+    print("file ")
+    for line in open(gen_path, 'r', encoding='utf-8'):
+        if re.search(regex, line):
+            c = line.split('=')[1].strip()
+            sys.stdout.write(c)
+
+else:
+    print("no file")
+
