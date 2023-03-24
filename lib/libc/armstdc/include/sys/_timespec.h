478ab0e63e70a6856fbf9e2be68bbc312eea5525
Kumar Gala <kumar.gala@intel.com>
2023-03-24 18:51:34 +0000

    simplify time

--- lib/libc/armstdc/include/sys/_timespec.h
+++ lib/libc/armstdc/include/sys/_timespec.h
@@ -7,8 +7,6 @@
 #ifndef ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_SYS__TIMESPEC_H_
 #define ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_SYS__TIMESPEC_H_
 
-#include <sys/_types.h>
-
 struct timespec {
 	time_t tv_sec;
 	long tv_nsec;
